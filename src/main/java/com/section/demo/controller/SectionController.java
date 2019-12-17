420b55e7f68f634d31d5a39f4b1aee007ea3dcfa
pyc6eh4uk <ruslan.klimov@gmail.com>
2019-12-17 21:28:37 +0300

    Set geoClasses

--- src/main/java/com/section/demo/controller/SectionController.java
+++ src/main/java/com/section/demo/controller/SectionController.java
@@ -4,9 +4,11 @@ import com.section.demo.entity.Section;
 import com.section.demo.repository.SectionRepository;
 import com.section.demo.service.SectionService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import javax.xml.ws.Response;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -26,8 +28,10 @@ public class SectionController {
     }
 
     @PostMapping("/sections/")
-    public Section createSection(@Valid @RequestBody Section section) {
-        return sectionRepository.save(section);
+    public ResponseEntity<Section> createSection(@Valid @RequestBody Section section) {
+        section = sectionRepository.save(section);
+        section.setGeoClasses(section.getGeoClasses());
+        return ResponseEntity.ok(section);
     }
 
     @DeleteMapping("/sections/{sectionId}")
